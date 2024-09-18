46cccc7323b0941a6564b4b603ad36436ce26d3c
Felicio <victor_felicio@hotmail.com>
2024-09-17 23:36:39 -0300

    feat: create portifolio section

--- src/app.routes.jsx
+++ src/app.routes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
+import Portifolio from './components/Portifolio/Portifolio';
 
 function AppRoutes() {
   const location = useLocation();
@@ -10,6 +11,7 @@ function AppRoutes() {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Hero />} />
         <Route path="/about" element={<About />} />
+        <Route path="/portifolio" element={<Portifolio />} />
       </Routes>
     </AnimatePresence>
   );
---
+++ src/components/Portifolio/Portifolio.jsx
@@ -0,0 +1,4 @@
+import './style/Portifolio.scss';
+export default function Portifolio() {
+  return <div className="portifolio-container">Portifolio</div>;
+}
