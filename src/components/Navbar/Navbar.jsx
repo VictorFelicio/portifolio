394cd824a8cab6a1c2d0094291f31b6ca1001e8c
Felicio <victor_felicio@hotmail.com>
2024-09-28 02:09:56 -0300

    feat: create responsive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,16 @@ import { Link, useLocation } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import './style/Navbar.scss';
 import { motion } from 'framer-motion';
+import menu_open from '/assets/menu_open.svg';
+import menu_close from '/assets/menu_close.svg';
+import { useState } from 'react';
 
 export default function Navbar() {
   const { pathname } = useLocation();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <motion.nav
@@ -14,16 +21,36 @@ export default function Navbar() {
       transition={{ duration: 1.5, ease: 'easeOut' }}
     >
       <Logo />
+      <img
+        src={menu_open}
+        alt=""
+        className="nav-menu-open"
+        onClick={toggleMenu}
+      />
 
-      <ul className="nav-menu">
+      <ul className={`nav-menu ${menuOpen ? 'open' : ''}`}>
+        <img
+          src={menu_close}
+          alt=""
+          className="nav-menu-close"
+          onClick={toggleMenu}
+        />
         <li>
-          <Link className={pathname === '/' ? 'active' : ''} to={'/'}>
+          <Link
+            className={pathname === '/' ? 'active' : ''}
+            to={'/'}
+            onClick={() => setMenuOpen(false)}
+          >
             Home
           </Link>
         </li>
         <li>
           {' '}
-          <Link className={pathname === '/about' ? 'active' : ''} to={'/about'}>
+          <Link
+            className={pathname === '/about' ? 'active' : ''}
+            to={'/about'}
+            onClick={() => setMenuOpen(false)}
+          >
             Sobre
           </Link>
         </li>
@@ -32,6 +59,7 @@ export default function Navbar() {
           <Link
             className={pathname === '/portifolio' ? 'active' : ''}
             to={'/portifolio'}
+            onClick={() => setMenuOpen(false)}
           >
             Portifólio
           </Link>
@@ -41,6 +69,7 @@ export default function Navbar() {
           <Link
             className={pathname === '/contact' ? 'active' : ''}
             to={'/contact'}
+            onClick={() => setMenuOpen(false)}
           >
             Contato
           </Link>
