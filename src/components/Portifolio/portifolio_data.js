1158102130ea211678168a48ec7d6687248d5614
Felicio <victor_felicio@hotmail.com>
2024-10-01 13:54:09 -0300

    feat: add new project to portifolio/biblioteca

--- src/components/Portifolio/portifolio_data.js
+++ src/components/Portifolio/portifolio_data.js
@@ -15,4 +15,12 @@ export const portifolio_data = [
     link_repo: 'https://github.com/VictorFelicio/DoctorCare-NLW',
     link_demo: 'https://jvfelicio.github.io/DoctorCare-NLW/',
   },
+  {
+    thumbnail: '/assets/img/biblioteca.webp',
+    title: 'Biblioteca Segura',
+    description:
+      'SPA de um CRUD para um desafio frontend, consiste em uma biblioteca para gerenciar livros e autores',
+    link_repo: 'https://github.com/VictorFelicio/dasafio-frontend',
+    link_demo: 'https://biblioteca-desafio-mu.vercel.app/',
+  },
 ];
